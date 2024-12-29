7f212768e4553d3baf65280a03aa733f92b3084e
keshav-610 <kesavaprakash1610@gmail.com>
2024-12-29 14:58:05 +0530

    education change

--- portfolio/src/contents/components/EducationCard.jsx
+++ portfolio/src/contents/components/EducationCard.jsx
@@ -1,39 +1,89 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material";
+
 const EducationCard = ({ logo, name, type, duration, percent }) => {
   return (
     <Box>
-      <Box sx={{
-        border: "1px solid grey", padding: "15px", borderRadius: "20px", transition: "transform 0.3s", cursor: "pointer", "&:hover": {
-          transform: "translateY(-10px)"
-        }
-      }}>
-        <Box sx={{ display: "flex", justifyContent: "space-between", flexDirection: "row", gap: "20px" }}>
-          <Box sx={{display:"flex", flexDirection:"column", justifyContent:"space-around"}}>
+      <Box
+        sx={{
+          border: "1px solid grey",
+          padding: {xs:"15px",md:"15px"},
+          borderRadius: "20px",
+          transition: "transform 0.3s",
+          cursor: "pointer",
+          "&:hover": {
+            transform: "translateY(10px)",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "row",
+            alignItems: "center",
+            gap:{xs:"10px",md:"20px"},
+          }}
+        >
+          <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "space-around",gap:"10px" }}>
             <Box>
-              <Typography variant="h4" sx={{ fontFamily: "SF Pro Display Black, sans-serif" }}>
+              <Typography
+                sx={{
+                  fontFamily: "SF Pro Display Heavy, sans-serif",
+                  fontWeight: 800,
+                  fontSize: { xs: "19px", md: "34px" }, 
+                }}
+              >
                 {name}
               </Typography>
+
             </Box>
-            <Box sx={{display:"flex",justifyContent:"space-around"}}>
-              <Typography  variant="h6" sx={{fontFamily: "SF Pro Display Medium, sans-serif", fontSize:"22px"}}>
+            <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+              <Typography
+                variant="h6"
+                sx={{
+                  fontFamily: "SF Pro Display Medium, sans-serif",
+                  fontSize: { xs: "17px", md: "22px" },
+                }}
+              >
                 {type}
               </Typography>
-              <Typography  variant="h6" sx={{fontFamily: "SF Pro Display Medium, sans-serif", fontSize:"22px", color:"#26eb33"}}>
+              <Typography
+                variant="h6"
+                sx={{
+                  fontFamily: "SF Pro Display Medium, sans-serif",
+                  fontSize: { xs: "17px", md: "22px" },
+                  color: "#26eb33",
+                }}
+              >
                 {percent}
               </Typography>
-              <Typography  variant="h6" sx={{fontFamily: "SF Pro Display Medium, sans-serif", fontSize:"22px", color:"#5c5c59"}}>
+              <Typography
+                variant="h6"
+                sx={{
+                  fontFamily: "SF Pro Display Medium, sans-serif",
+                  fontSize: { xs: "17px", md: "22px" },
+                  color: "#5c5c59",
+                }}
+              >
                 {duration}
               </Typography>
             </Box>
           </Box>
           <Box>
-            <img src={logo} width={100} />
+            <img
+              src={logo}
+              style={{
+                width: "90%",
+                maxWidth: "100px",
+                maxHeight: "100px",
+              }}
+              alt={`${name} logo`}
+            />
           </Box>
         </Box>
-
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default EducationCard
+export default EducationCard;
